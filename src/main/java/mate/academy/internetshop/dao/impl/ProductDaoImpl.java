d0a08d64850303bfdbb50ea3d892be37b33c28e3
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-04-23 11:32:17 +0300

    hw_14_1

---
+++ src/main/java/mate/academy/internetshop/dao/impl/ProductDaoImpl.java
@@ -0,0 +1,56 @@
+package mate.academy.internetshop.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.internetshop.dao.ProductDao;
+import mate.academy.internetshop.db.Storage;
+import mate.academy.internetshop.lib.Dao;
+import mate.academy.internetshop.model.Product;
+
+@Dao
+public class ProductDaoImpl implements ProductDao {
+
+    @Override
+    public Product create(Product product) {
+        Storage.addProduct(product);
+        return product;
+    }
+
+    @Override
+    public Optional<Product> get(Long id) {
+        return Storage
+                .products
+                .stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return Storage.products;
+    }
+
+    @Override
+    public Product update(Product product) {
+        Storage
+                .products
+                .stream()
+                .filter(p -> p.getId().equals(product.getId()))
+                .forEach((p) -> {
+                    p.setName(product.getName());
+                    p.setPrice(product.getPrice());
+                });
+        return product;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        Product product = Storage
+                .products
+                .stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+        return Storage.products.remove(product);
+    }
+}
---
+++ src/main/java/mate/academy/internetshop/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,7 @@
+package mate.academy.internetshop.service.impl;
+
+import mate.academy.internetshop.lib.Service;
+
+@Service
+public class ShoppingCartServiceImpl {
+}
