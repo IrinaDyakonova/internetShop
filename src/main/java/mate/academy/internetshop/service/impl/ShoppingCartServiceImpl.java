3ccccf9186cfafdda5d3b2b4872f3ec01f8905b4
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-04-27 08:14:31 +0300

    hw_15

--- src/main/java/mate/academy/internetshop/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/internetshop/service/impl/ShoppingCartServiceImpl.java
@@ -49,4 +49,29 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         return shoppingCart
                 .getProducts();
     }
+
+    @Override
+    public ShoppingCart create(ShoppingCart shoppingCart) {
+        return null;
+    }
+
+    @Override
+    public ShoppingCart get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<ShoppingCart> getAll() {
+        return null;
+    }
+
+    @Override
+    public ShoppingCart update(ShoppingCart element) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
 }
