cd5d3bb79a6ca2b7f7cb400f1131c8b6e61e3f0a
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-05-19 15:13:28 +0300

    hw_25

--- src/main/java/mate/academy/internetshop/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/internetshop/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import mate.academy.internetshop.lib.Inject;
 import mate.academy.internetshop.lib.Service;
 import mate.academy.internetshop.model.User;
 import mate.academy.internetshop.service.UserService;
+import mate.academy.internetshop.util.HashUtil;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -18,6 +19,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public User create(User user)
             throws DataProcessingException {
+        user.setSalt(HashUtil.getSalt());
+        user.setPassword(HashUtil.hashPassword(user.getPassword(), user.getSalt()));
         return userDao.create(user);
     }
 
